6c995814021745821f20e8ad7a0a5343d689f75f
Douglas Braun <dbraun@instream.net>
2015-04-15 13:37:59 -0700

    ask new page

--- R/HistPlot.R
+++ R/HistPlot.R
@@ -36,7 +36,7 @@ hist_records <- function(dataset, day_one=NULL, site=NULL, year=NULL) {
       xaxs = "i", 
       yaxs = "i",
       cex = 1.5)
-  devAskNewPage(ask = TRUE)
+ 
   
   no_events <- plyr::ddply(filter_(d, ~description == "E"), c("channel"), function(x) {
     hist(x$signal, breaks = seq(0, 130, 5), xlim = c(0, 130), main = "", ylab = "", 
@@ -59,6 +59,7 @@ hist_records <- function(dataset, day_one=NULL, site=NULL, year=NULL) {
   #pdf(paste(getwd(),"/", site, year, "UpsbyChannel.pdf", sep = ""),
   #    height = 10,
   #    width = 10)
+  devAskNewPage(ask = TRUE)
   dev.new()
   par(mfrow = c(length(unique(d$channel)), 1), 
       mar = c(4, 3, 1, 1), 
@@ -67,7 +68,6 @@ hist_records <- function(dataset, day_one=NULL, site=NULL, year=NULL) {
       xaxs = "i", 
       yaxs = "i", 
       cex = 1.5)
-  devAskNewPage(ask = TRUE)
   
   no_up <- plyr::ddply(filter_(d, ~description == "U"), c("channel"), function(x) {
     hist(x$signal, breaks = seq(0, 130, 5), xlim = c(0, 130), main = "", ylab = "", 
@@ -96,7 +96,6 @@ hist_records <- function(dataset, day_one=NULL, site=NULL, year=NULL) {
       xaxs = "i", 
       yaxs = "i",
       cex = 1.5)
-  devAskNewPage(ask = TRUE)
   
   no_down <- plyr::ddply(filter_(d, ~description == "D"), c("channel"), function(x) {
     hist(x$signal, breaks = seq(0, 130, 5), xlim = c(0, 130), main = "", ylab = "", 
