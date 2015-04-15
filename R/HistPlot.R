96e25ada132e2bf6c0437bc3f29d300f8a82005a
Douglas Braun <dbraun@instream.net>
2015-04-15 13:43:01 -0700

    changed par

--- R/HistPlot.R
+++ R/HistPlot.R
@@ -24,6 +24,15 @@ hist_records <- function(dataset, day_one=NULL, site=NULL, year=NULL) {
   d1 <- dplyr::filter_(dataset, ~jday >= day_one)
   d <- dplyr::select(d1, channel, description, signal)
   
+  
+  par1 <- list(mfrow = c(length(unique(d$channel)), 1), 
+      mar = c(4, 3, 1, 1), 
+      oma = c(2, 2, 0.5, 0), 
+      las = 1, 
+      xaxs = "i", 
+      yaxs = "i", 
+      cex = 1.5)
+  
   # get rid of pdf function.
   #pdf(paste(getwd(),"/", site, year, "EventsbyChannel.pdf", sep = ""),
   #    height = 10,
@@ -68,7 +77,7 @@ hist_records <- function(dataset, day_one=NULL, site=NULL, year=NULL) {
   #    xaxs = "i", 
   #    yaxs = "i", 
   #    cex = 1.5)
-  
+  par(par1)
   no_up <- plyr::ddply(filter_(d, ~description == "U"), c("channel"), function(x) {
     hist(x$signal, breaks = seq(0, 130, 5), xlim = c(0, 130), main = "", ylab = "", 
          xlab = paste("Channel ", x$channel[1], sep = ""), col = "grey60")
