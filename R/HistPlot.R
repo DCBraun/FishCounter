046fb981cbf82bdc33c7d51c267894b4ce6b5edb
Douglas Braun <dbraun@instream.net>
2015-04-24 11:25:46 -0700

    added last_day subset

--- R/HistPlot.R
+++ R/HistPlot.R
@@ -3,12 +3,12 @@
 #' This function plots historgrams of up, down and event counts for Logie counter data by channel as specified by the user.
 #' @param dataset This is the dataset used to create the histograms.
 #' @param direction This is the type of record ("U", "D", or "E") to be plotted.
-#' @param day_one This is the first day of the dataset you want to use, defaults to the first day found in the dataset. This parameter needs to be specified in julian day format.
+#' @param first_day This is the first day of the dataset you want to use, defaults to the first day found in the dataset. This parameter needs to be specified in julian day format.
 #' @return Generates a histograms of peak signal size for either up counts, down counts and events for each counter channel.
 #' @keywords Histogram
 #' @export
 
-hist_records <- function(dataset, direction, day_one=NULL) {
+hist_records <- function(dataset, direction, first_day = NULL, last_day = NULL) {
   if(missing(direction)) {
     stop(paste("Need to specify the direction as character:", "U", "D", "E"))
   }
@@ -26,11 +26,14 @@ hist_records <- function(dataset, direction, day_one=NULL) {
   }
   
   dataset$jday <- strptime(dataset$date, '%Y-%m-%d')$yday
-  if(is.null(day_one)) {
-    day_one <- min(dataset$jday)
+  if(is.null(first_day)) {
+    first_day <- min(dataset$jday)
+  }
+  if(is.null(last_day)) {
+    last_day <- min(dataset$jday)
   }
   
-  d1 <- dplyr::filter_(dataset, ~jday >= day_one)
+  d1 <- dplyr::filter_(dataset, ~jday >= first_day, ~jday >= last_day)
   d <- dplyr::select(d1, channel, description, signal)
   
   #plot.new()
